612210b844c93a4ec2a34f61e68897549cb57d16
Vadim <cloudplaid@yandex.ru>
2024-08-12 19:08:48 +0300

    Updates

--- src/functions/storage.ts
+++
@@ -1,7 +0,0 @@
-export function storageSet<T>(payload: T, key: string) {
-  localStorage.setItem(key, JSON.stringify(payload));
-}
-
-export function storageGet<T>(key: string): T {
-  return JSON.parse(localStorage.getItem(key) || "[]");
-}
--- src/types/dropdownValues.ts
+++
@@ -1,4 +0,0 @@
-export type DropdownValues = {
-  name: string;
-  value: string;
-};
